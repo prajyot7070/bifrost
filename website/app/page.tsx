3f7406c9db1d7826e06b4fa4e41ebe3c1f6932e5
prajyot7070 <maneprajyot756@gmail.com>
2025-07-15 12:13:28 +0530

    updated package files and landing page.tsx

--- website/app/page.tsx
+++ website/app/page.tsx
@@ -1,6 +1,33 @@
-import ComingSoon from "./coming-soon/page";
+import { BackgroundBeams } from "@/components/background-beams";
+import { HeroText } from "@/components/HeroText";
+import { Navbar } from "@/components/Navbar";
+import { TerminalImage } from "@/components/TerminalImage";
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <ComingSoon/>
+  <div>
+    {/* Navbar */}
+    <Navbar />
+
+    {/* Hero Section Text */}
+    <HeroText />
+
+    {/*Background beams*/}
+    <BackgroundBeams />
+    
+
+  {/* Terminal image*/}
+    <div className="absolute bottom-[-50vh] left-1/2 transform -translate-x-1/2 z-20">
+      <TerminalImage
+        imagePath="/terminal.png"
+        width={1080}
+        height={650}
+        alt="Sample img"
+      />
+    </div>
+
+  </div>
   );
 }
+
