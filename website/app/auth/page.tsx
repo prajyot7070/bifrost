3ab6d78c58ae4a015156f43bfca58a63a77307a2
prajyot7070 <maneprajyot756@gmail.com>
2025-07-16 15:05:13 +0530

    auth page

---
+++ website/app/auth/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import AuthForm from "@/components/AuthForm";
+import { BackgroundBeams } from "@/components/background-beams";
+
+export default function AuthPage() {
+  return (
+    <div>
+      <BackgroundBeams />
+      <AuthForm />
+    </div>
+  );
+}
