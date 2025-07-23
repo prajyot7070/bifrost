2b8a32ac2aeaf4f6394bb248c78f2d3fc696bb2c
prajyot7070 <maneprajyot756@gmail.com>
2025-07-23 14:11:47 +0530

    Implemented the bridge command

--- client/cmd/bridge.go
+++ client/cmd/bridge.go
@@ -1,40 +1,61 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+	"log"
 
 	"github.com/spf13/cobra"
+  "bifrost/client/internal"
 )
+
+var port int
+const serverAddr string = "52.7.141.2:8080"
 
 // bridgeCmd represents the bridge command
 var bridgeCmd = &cobra.Command{
 	Use:   "bridge",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Expose your local service to the internet",
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("bridge called")
+    apiKey := loadApiKey()
+    if apiKey == "" {
+      fmt.Println("Counldn't load Apikey \n Run `bifrost auth --key <API_KEY>` first")
+      return
+    }
+    client := client.NewTunnelClient(serverAddr, port, apiKey)
+		// Handle shutdown signals
+		shutdownChan := make(chan os.Signal, 1)
+		signal.Notify(shutdownChan, syscall.SIGINT, syscall.SIGTERM)
+		go func() {
+			<-shutdownChan
+			log.Println("Interrupt received, shutting down...")
+			client.Close()
+			os.Exit(0)
+		}()
+
+		if err := client.Start(); err != nil {
+			log.Fatalf("❌ Failed to start tunnel: %v", err)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(bridgeCmd)
+}
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// bridgeCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// bridgeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+func loadApiKey() string {
+  path := getConfigPath()
+  data, err := os.ReadFile(path)
+  if err != nil {
+    return ""
+  }
+  var config map[string]string
+  json.Unmarshal(data, &config)
+  return config["apiKey"]
 }
