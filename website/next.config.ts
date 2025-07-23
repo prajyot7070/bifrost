026813419b06102e57a98cf3f14c43d361b0dfe3
prajyot7070 <maneprajyot756@gmail.com>
2025-07-23 21:05:09 +0530

    Added setting to remove console statments

--- website/next.config.ts
+++ website/next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   transpilePackages: ['motion-dom'],
+  compiler: {
+    removeConsole: true,
+  },
 };
 
 export default nextConfig;
