b1b0e312e4dc3ca0f1f27cbf2435c20a21c7feac
prajyot7070 <maneprajyot756@gmail.com>
2025-07-24 18:12:08 +0530

    Added Google analytics

--- website/app/layout.tsx
+++ website/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
 import Script from "next/script";
+import { GoogleTagManager } from '@next/third-parties/google';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,30 +30,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-    <head>
-        {/* Google Analytics Script */}
-        <Script id="gtm-init" strategy="afterInteractive">
-        {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-WHV6FQTX');
-`}
-        </Script>
-      </head>
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-      {/*Google Tag Manager (noscript)
-      <noscript>
-        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WHV6FQTX"
-          height="0" width="0" style={{display:'none', visibility:'hidden'}}>
-        </iframe>
-      </noscript>
-       End Google Tag Manager (noscript)*/}       
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
       {children}
         <Toaster richColors position="bottom-right" />
       </body>
+      { process.env.NEXT_PUBLIC_GTM_ID && <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} />}
     </html>
   );
 }
